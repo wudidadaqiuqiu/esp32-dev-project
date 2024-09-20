d890838b0185752250b072ccbfec4e6f671caceb
xy <wudidadaqiuqiu@qq.com>
2024-09-20 14:11:51 +0800

    电平触发发送

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,21 @@ FsFile file;  // 录音文件
 MyI2S mi;
 const int record_time = 10;  // second
 const char filename[] = "/test.wav";
-const char *ssid = "HITSZ";
-const char *password = "";
+const char *ssid = "linux";
+const char *password = "12345678";
+
 
 const int waveDataSize = record_time * 88200;
 int32_t communicationData[1024];     //接收缓冲区
 char partWavData[1024];
-IPAddress serverIP(10, 250, 145, 73);
+IPAddress serverIP(192, 168, 248, 110);
 uint16_t serverPort = 3334;
-// WiFiClient client;
+uint16_t server_end_port = 3335;
+
+WiFiClient client;
+WiFiClient end_client;
+
+const int gpio_ = 13;
 
 void wifi_setup() {
   WiFi.begin(ssid, password);
@@ -45,8 +51,25 @@ void record_plus() {
   EEPROM.commit();
 }
 
+void end_record() {
+  if (!end_client.connected()) {
+    while (!end_client.connect(serverIP, server_end_port, 1000)) {
+      Serial.println("end port connecting.");
+    }
+  }
+  Serial.println("end record");
+  end_client.write("----------");
+  end_client.flush();
+  end_client.stop();
+}
+
 void save_record() {
   String path = "/record" + String(EEPROM.read(0)) + ".wav";
+  if (!client.connected()) {
+    while (!client.connect(serverIP, serverPort, 1000)) {
+      Serial.println("client connecting.");
+    }
+  }
 
   //删除并创建文件
   sd.remove(path.c_str());
@@ -60,7 +83,7 @@ void save_record() {
   auto header = CreateWaveHeader(1, 44100, 16);
   header.riffSize = waveDataSize + 44 - 8;
   header.dataSize = waveDataSize;
-  // client.write((uint8_t*)&header, 44);
+  client.write((uint8_t*)&header, 44);
   file.write(&header, 44);
 
   // if(!mi.InitInput(I2S_BITS_PER_SAMPLE_32BIT, 17, 21, 4))
@@ -72,7 +95,7 @@ void save_record() {
   Serial.println("start");
 
   for (int j = 0; j < waveDataSize/1024; ++j) {
-    Serial.printf("read\n");
+    // Serial.printf("read\n");
     auto sz = mi.Read((char*)communicationData, 4096);
     
 
@@ -87,9 +110,12 @@ void save_record() {
       }
     }
     file.write((const byte*)partWavData, 1024);
-    // client.write((const byte*)partWavData, 1024);
+    client.write((const byte*)partWavData, 1024);
   }
   file.close();
+  client.flush();
+  client.stop();
+  Serial.println("finish save");
   record_plus();
 }
 void setup() {
@@ -97,9 +123,11 @@ void setup() {
   reset_record();
   delay(500);
   wifi_setup();
-  // while (!client.connect(serverIP, serverPort, 1000)) {
-  //   Serial.println("client connecting.");
+
+  // while (!end_client.connect(serverIP, server_end_port, 1000)) {
+  //   Serial.println("end port connecting.");
   // }
+  
   // Serial.println("connected");
   // 初始化SD卡
   if(!sd.begin(SdSpiConfig(5, DEDICATED_SPI, 18000000)))
@@ -154,12 +182,31 @@ void setup() {
   // client.flush();
   // client.stop();
   
+  pinMode(gpio_, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(1);
+  delay(500);
   static int cnt = 0;
-  if (cnt++ <= 2)
+  // 读取引脚的电平状态
+  int level = digitalRead(gpio_);
+  static int last_level;
+  // 输出引脚的电平状态
+  Serial.print("GPIO pin ");
+  Serial.print(gpio_);
+  Serial.print(" level: ");
+  Serial.println(level);
+
+  
+  if (level) {
+    
     save_record();
+    
+  } else if (last_level) {
+    end_record();
+    last_level = 0;
+  }
+  last_level = level;
+
 }
